029f23dbaddea2dcd12cb7e574bc8fdb4b7d3a9d
Name <Email>
2017-04-29 03:13:34 -0300

    maven convert

--- src/TestJDBC.java
+++ src/TestJDBC.java
@@ -1,54 +1,40 @@
-import java.sql.CallableStatement;
-import java.sql.Connection;
+
+
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 
-/*
- * Created on 11/09/2006
- *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
+public class TestJDBC {
 
-/**
- * @author Claudio
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
- */
-public class TestJDBC
-{
-	public static void main(String[] args)
-	{
-		try
-		{
-			System.out.println("Cargando el Driver JDBC");
-			Class.forName("com.mysql.jdbc.Driver");
-			System.out.println("Creando una nueva conexión JDBC con la DB");
-			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/productosdb?user=root&password=lococaz");
-			CallableStatement callableStatement = conexion.prepareCall("{CALL productosdb.SP_ListadoPorCostos(?)}");
-			callableStatement.setDouble(1,200);
-			ResultSet resultSet = callableStatement.executeQuery();
-			
-			while(resultSet.next())
-			{
-				System.out.println(resultSet.getString(2));
-			}
+  public static void main(String[] argv) {
 
-			conexion.close();
-		}
-		catch (ClassNotFoundException e)
-		{
-			System.out.println("El Driver JDBC de HSQL no fue encontrado");
-			e.printStackTrace();
-		}
-		catch (SQLException e)
-		{
-			System.out.println("Error");
-			e.printStackTrace();
-		}
-		System.exit(0);
+	System.out.println("-------- MySQL JDBC Connection Testing ------------");
+
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+	} catch (ClassNotFoundException e) {
+		System.out.println("Where is your MySQL JDBC Driver?");
+		e.printStackTrace();
+		return;
 	}
+
+	System.out.println("MySQL JDBC Driver Registered!");
+	Connection connection = null;
+
+	try {
+		connection = DriverManager
+		.getConnection("jdbc:mysql://localhost:3306/sys","root", "qwerty");
+
+	} catch (SQLException e) {
+		System.out.println("Connection Failed! Check output console");
+		e.printStackTrace();
+		return;
+	}
+
+	if (connection != null) {
+		System.out.println("You made it, take control your database now!");
+	} else {
+		System.out.println("Failed to make connection!");
+	}
+  }
 }
